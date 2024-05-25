311fe6484b069cf7cba14fe5f442cba602535a90
Prathibha Ratnayake <prsthibha@gmail.com>
2024-05-25 12:24:24 +0530

    add shop page and refill hearts functionality

--- actions/user-progress.ts
+++ actions/user-progress.ts
@@ -8,6 +8,8 @@ import { and, eq } from 'drizzle-orm';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+const POINTS_TO_REFILL = 10;
+
 /**
  * Updates or inserts user progress for a given course.
  *
@@ -117,4 +119,33 @@ export const reduceHearts = async (challengeId: number) => {
   revalidatePath('/quests');
   revalidatePath('/leaderboard');
   revalidatePath(`/lesson/${lessonId}`);
+};
+
+export const refillHearts = async () => {
+  const currentUserProgress = await getUserProgress();
+
+  if (!userProgress) {
+    throw new Error('User progress not found');
+  }
+
+  if (currentUserProgress?.hearts === 5) {
+    throw new Error('Hearts are already full');
+  }
+
+  if (currentUserProgress && currentUserProgress?.points < POINTS_TO_REFILL) {
+    throw new Error('Not enough points');
+  }
+
+  await db
+    .update(userProgress)
+    .set({
+      hearts: 5,
+      points: currentUserProgress?.points! - POINTS_TO_REFILL,
+    })
+    .where(eq(userProgress.userId, currentUserProgress?.userId!));
+
+  revalidatePath('/shop');
+  revalidatePath('/learn');
+  revalidatePath('/quests');
+  revalidatePath('/leaderboard');
 };

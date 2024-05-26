11992ef6ec69dd5cf0cda6a49e9c843fa21d8986
Prathibha Ratnayake <prsthibha@gmail.com>
2024-05-26 10:33:28 +0530

    add leaderboard page

--- database/queries.ts
+++ database/queries.ts
@@ -260,3 +260,23 @@ export const getUserSubscription = cache(async () => {
     isActive: !!isActive,
   };
 });
+
+export const getTopTenUsers = cache(async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return [];
+  }
+
+  const data = await db.query.userProgress.findMany({
+    orderBy: (userProgress, { desc }) => [desc(userProgress.points)],
+    limit: 10,
+    columns: {
+      userId: true,
+      username: true,
+      userImageSrc: true,
+      points: true,
+    },
+  });
+  return data;
+});
